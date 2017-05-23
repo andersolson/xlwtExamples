1478433f8eab59a85524155e5cd7201f923f163f
Anders Olson <aolson@digitalglobe.com>
2017-05-23 10:14:50 -0600

    Loaded examples
    Loaded examples of excel formating documents.

--- xlwt_Practice.py
+++ xlwt_Practice.py
@@ -1,45 +1,45 @@
 #excelOutput = "C:\\Users\\aolson\\Documents\\Working\\Plus_Metro\\PYTHON\\Reports\\"
-excelOutput = r"C:\Users\aolson\Documents\GitHub\xlwtExamples\"
+excelOutput = "C:\\Users\\aolson\\Documents\\GitHub\\xlwtExamples\\"
 
-#===============================#
-# Make a reference table for font color
-#===============================#
+##===============================#
+## Make a reference table for font color
+##===============================#
 
-from xlwt import *
+#from xlwt import *
 
-font0 = Font()
-font0.name = 'Times New Roman'
-font0.bold = True
+#font0 = Font()
+#font0.name = 'Times New Roman'
+#font0.bold = True
 
-style0 = XFStyle()
-style0.font = font0
+#style0 = XFStyle()
+#style0.font = font0
 
-wb = Workbook()
-ws0 = wb.add_sheet('data')
+#wb = Workbook()
+#ws0 = wb.add_sheet('data')
 
-ws0.write(1, 1, 'Test', style0)
+#ws0.write(1, 1, 'Test', style0)
 
-for i in range(0, 0x53):
-   fnt = Font()
-   fnt.name = 'Arial'
-   fnt.colour_index = i
-   fnt.outline = True
+#for i in range(0, 0x53):
+   #fnt = Font()
+   #fnt.name = 'Arial'
+   #fnt.colour_index = i
+   #fnt.outline = True
 
-   borders = Borders()
-   borders.left = i
+   #borders = Borders()
+   #borders.left = i
 
-   style = XFStyle()
-   style.font = fnt
-   style.borders = borders
+   #style = XFStyle()
+   #style.font = fnt
+   #style.borders = borders
 
-   ws0.write(i, 2, 'colour', style)
-   ws0.write(i, 3, hex(i), style0)
+   #ws0.write(i, 2, 'colour', style)
+   #ws0.write(i, 3, hex(i), style0)
 
-wb.save(excelOutput + 'colours.xls')
+#wb.save(excelOutput + 'coloursExample.xls')
 
-#===============================#
-# Example for making and naming a sheet
-#===============================#
+##===============================#
+## Example for making and naming a sheet
+##===============================#
 
 #from xlwt import *
 #
@@ -48,9 +48,9 @@ wb.save(excelOutput + 'colours.xls')
 #ws = w.add_sheet('AU')
 #w.save(excelOutput + 'country.xls')
 
-#===============================#
-# Make an example table using easyxf
-#===============================#
+##===============================#
+## Make an example table using easyxf
+##===============================#
 
 #import xlwt
 #from datetime import datetime
@@ -68,12 +68,12 @@ wb.save(excelOutput + 'colours.xls')
 #ws.write(2, 1, 1)
 #ws.write(2, 2, xlwt.Formula("A3+B3"))
 
-#wb.save(excelOutput + 'example.xls')
+#wb.save(excelOutput + 'xlwtExample.xls')
 
-#===============================#
-# Make a reference table of all the 
-# number formats for a cell in excel
-#===============================#
+##===============================#
+## Make a reference table of all the 
+## number formats for a cell in excel
+##===============================#
 
 #from xlwt import *
 
@@ -130,12 +130,12 @@ wb.save(excelOutput + 'colours.xls')
 
     #i += 1
 
-#w.save(excelOutput + 'num_formats.xls')
+#w.save(excelOutput + 'num_formatsExample.xls')
 
-#===============================#
-# Make a huge excel table and 
-# and track the time it takes to make
-#===============================#
+##===============================#
+## Make a huge excel table and 
+## and track the time it takes to make
+##===============================#
 
 #from time import *
 #from xlwt.Workbook import *
@@ -154,10 +154,10 @@ wb.save(excelOutput + 'colours.xls')
 
 #print("Filling...")
 #for col in xrange(colcount):
-    ##print("[%d]" % col, end=' ') 
-    #for row in xrange(rowcount):
-        ##ws0.write(row, col, "BIG(%d, %d)" % (row, col))
-        #ws0.write(row, col, "BIG")
+   ##print("[%d]" % col, end=' ') 
+   #for row in xrange(rowcount):
+      ##ws0.write(row, col, "BIG(%d, %d)" % (row, col))
+      #ws0.write(row, col, "BIG")
 
 #t1 = time() - t0
 #print("\nsince starting elapsed %.2f s" % (t1))
@@ -168,10 +168,10 @@ wb.save(excelOutput + 'colours.xls')
 #t2 = time() - t0
 #print("since starting elapsed %.2f s" % (t2))
 
-#===============================#
-# Make a reference sheet for all 
-# possible border styles.
-#===============================#
+##===============================#
+##Make a reference sheet for all 
+##possible border styles.
+##===============================#
 
 #from xlwt import *
 
@@ -182,7 +182,6 @@ wb.save(excelOutput + 'colours.xls')
 
 #style0 = XFStyle()
 #style0.font = font0
-
 
 #wb = Workbook()
 #ws0 = wb.add_sheet('0')
@@ -204,11 +203,11 @@ wb.save(excelOutput + 'colours.xls')
 
 #ws0.write_merge(5, 8, 6, 10, "")
 
-#wb.save(excelOutput + 'blanks.xls')
+#wb.save(excelOutput + 'borderExample.xls')
 
-#===============================#
-# Not sure what this does...
-#===============================#
+##===============================#
+## Not sure what this does...
+##===============================#
 
 #from xlwt import *
 
@@ -224,10 +223,10 @@ wb.save(excelOutput + 'colours.xls')
    #ws.col(i).width = 0x0d00 + i
 #w.save(excelOutput + 'col_width.xls')
 
-#===============================#
-# Make a reference table of all 
-# possible date formats.
-#===============================#
+##===============================#
+## Make a reference table of all 
+## possible date formats.
+##===============================#
 
 #from xlwt import *
 #from datetime import datetime
@@ -261,12 +260,12 @@ wb.save(excelOutput + 'colours.xls')
 
    #i += 1
 
-#w.save(excelOutput + 'dates.xls')
+#w.save(excelOutput + 'datesExample.xls')
 
-#===============================#
-# Make a table with examples of how to 
-# create formulas in each cell
-#===============================#
+##===============================#
+## Make a table with examples of how to 
+## create formulas in each cell
+##===============================#
 
 #from xlwt import *
 
@@ -310,12 +309,12 @@ wb.save(excelOutput + 'colours.xls')
 #ws.write(11, 2, Formula('FALSE'))
 #ws.write(12, 3, Formula('IF(A1>A2;3;"hkjhjkhk")'))
 
-#w.save(excelOutput + 'formulas.xls')
+#w.save(excelOutput + 'formulasExample.xls')
 
-#===============================#
-# Make a table that demonstrates how 
-# to write a value to a specific cell using col/row numbers
-#===============================#
+##===============================#
+## Make a table that demonstrates how 
+## to write a value to a specific cell using col/row numbers
+##===============================#
 
 #from xlwt import *
 
@@ -337,11 +336,11 @@ wb.save(excelOutput + 'colours.xls')
 #ws.write(2, 2, -12345678.90780980)
 #ws.write(3, 2, -123456.78)
 
-#w.save(excelOutput + 'numbers.xls')
+#w.save(excelOutput + 'numbersExample.xls')
 
-#===============================#
-# Demonstrates how to freeze different panes.
-#===============================#
+##===============================#
+## Demonstrates how to freeze different panes.
+##===============================#
 
 #from xlwt import *
 
@@ -395,11 +394,11 @@ wb.save(excelOutput + 'colours.xls')
 #ws6.vert_split_pos = 40
 #ws4.vert_split_first_visible = 2
 
-#w.save(excelOutput + 'panes.xls')
+#w.save(excelOutput + 'panesExample.xls')
 
-#===============================#
-# Demonstrates how to change the height of rows.
-#===============================#
+##===============================#
+## Demonstrates how to change the height of rows.
+##===============================#
 
 #from xlwt import *
 
@@ -413,55 +412,55 @@ wb.save(excelOutput + 'colours.xls')
    #style.font = fnt
    #ws.write(i, 1, 'Test')
    #ws.row(i).set_style(style)
-#w.save(excelOutput + 'row_styles.xls')
+#w.save(excelOutput + 'row_stylesExample.xls')
 
 #===============================#
 # Write an XLS file with a single worksheet, containing
 # a heading row and some rows of data.
 #===============================#
 
-#import xlwt
-#import datetime
-#ezxf = xlwt.easyxf
+import xlwt
+import datetime
+ezxf = xlwt.easyxf
 
-#def write_xls(file_name, sheet_name, headings, data, heading_xf, data_xfs):
-   #book = xlwt.Workbook()
-   #sheet = book.add_sheet(sheet_name)
-   #rowx = 0
-   #for colx, value in enumerate(headings):
-      #sheet.write(rowx, colx, value, heading_xf)
-   #sheet.set_panes_frozen(True) # frozen headings instead of split panes
-   #sheet.set_horz_split_pos(rowx+1) # in general, freeze after last heading row
-   #sheet.set_remove_splits(True) # if user does unfreeze, don't leave a split there
-   #for row in data:
-      #rowx += 1
-      #for colx, value in enumerate(row):
-         #sheet.write(rowx, colx, value, data_xfs[colx])
-   #book.save(file_name)
+def write_xls(file_name, sheet_name, headings, data, heading_xf, data_xfs):
+   book = xlwt.Workbook()
+   sheet = book.add_sheet(sheet_name)
+   rowx = 0
+   for colx, value in enumerate(headings):
+      sheet.write(rowx, colx, value, heading_xf)
+   sheet.set_panes_frozen(True) # frozen headings instead of split panes
+   sheet.set_horz_split_pos(rowx+1) # in general, freeze after last heading row
+   sheet.set_remove_splits(True) # if user does unfreeze, don't leave a split there
+   for row in data:
+      rowx += 1
+      for colx, value in enumerate(row):
+         sheet.write(rowx, colx, value, data_xfs[colx])
+   book.save(file_name)
 
-#if __name__ == '__main__':
-   #import sys
-   #mkd = datetime.date
-   #hdngs = ['Date', 'Stock Code', 'Quantity', 'Unit Price', 'Value', 'Message']
-   #kinds =  'date    text          int         price         money    text'.split()
-   #data = [
-      #[mkd(2007, 7, 1), 'ABC', 1000, 1.234567, 1234.57, ''],
-      #[mkd(2007, 12, 31), 'XYZ', -100, 4.654321, -465.43, 'Goods returned'],
-      #] + [
-         #[mkd(2008, 6, 30), 'PQRCD', 100, 2.345678, 234.57, ''],
-         #] * 100
+if __name__ == '__main__':
+   import sys
+   mkd = datetime.date
+   hdngs = ['Date', 'Stock Code', 'Quantity', 'Unit Price', 'Value', 'Message']
+   kinds =  'date    text          int         price         money    text'.split()
+   data = [
+      [mkd(2007, 7, 1), 'ABC', 1000, 1.234567, 1234.57, ''],
+      [mkd(2007, 12, 31), 'XYZ', -100, 4.654321, -465.43, 'Goods returned'],
+      ] + [
+         [mkd(2008, 6, 30), 'PQRCD', 100, 2.345678, 234.57, ''],
+         ] * 100
 
-   #heading_xf = ezxf('font: bold on; align: wrap on, vert centre, horiz center')
-   #kind_to_xf_map = {
-      #'date': ezxf(num_format_str='m/d/yyyy'),
-      ##'date': ezxf(num_format_str='mm/dd/yyyy'),
-      ##'date': ezxf(num_format_str='mm-dd-yyyy'),
-      ##'date': ezxf(num_format_str='yyyy-mm-dd'),
-      #'int': ezxf(num_format_str='#,##0'),
-      #'money': ezxf('font: italic on; pattern: pattern solid, fore-colour grey25',
-                    #num_format_str='$#,##0.00'),
-      #'price': ezxf(num_format_str='#0.000000'),
-      #'text': ezxf(),
-   #}
-   #data_xfs = [kind_to_xf_map[k] for k in kinds]
-   #write_xls(excelOutput + 'xlwt_easyxf_simple_demo.xls', 'Demo', hdngs, data, heading_xf, data_xfs)
+   heading_xf = ezxf('font: bold on; align: wrap on, vert centre, horiz center')
+   kind_to_xf_map = {
+      'date': ezxf(num_format_str='m/d/yyyy'),
+      #'date': ezxf(num_format_str='mm/dd/yyyy'),
+      #'date': ezxf(num_format_str='mm-dd-yyyy'),
+      #'date': ezxf(num_format_str='yyyy-mm-dd'),
+      'int': ezxf(num_format_str='#,##0'),
+      'money': ezxf('font: italic on; pattern: pattern solid, fore-colour grey25',
+                    num_format_str='$#,##0.00'),
+      'price': ezxf(num_format_str='#0.000000'),
+      'text': ezxf(),
+   }
+   data_xfs = [kind_to_xf_map[k] for k in kinds]
+   write_xls(excelOutput + 'xlwt_easyxf_simple_demo.xls', 'Demo', hdngs, data, heading_xf, data_xfs)
