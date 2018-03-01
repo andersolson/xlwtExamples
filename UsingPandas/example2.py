39ef4cd9610444aa78feca9bfa3d7067f14febd0
Anders Olson <aolson@digitalglobe.com>
2018-03-01 13:17:55 -0700

    create line
    create a line chart

--- UsingPandas/example2.py
+++ UsingPandas/example2.py
@@ -4,7 +4,7 @@ import pandas as pd
 df = pd.DataFrame({'Data': [10, 20, 30, 20, 15, 30, 45]})
 
 # Create a Pandas Excel writer using XlsxWriter as the engine.
-writer = pd.ExcelWriter('pandas_simple.xlsx', engine='xlsxwriter')
+writer = pd.ExcelWriter('pandas_chart.xlsx', engine='xlsxwriter')
 
 # Convert the dataframe to an XlsxWriter Excel object.
 df.to_excel(writer, sheet_name='Sheet1')
@@ -12,3 +12,38 @@ df.to_excel(writer, sheet_name='Sheet1')
 # Get the xlsxwriter objects from the dataframe writer object.
 workbook  = writer.book
 worksheet = writer.sheets['Sheet1']
+
+# Create a chart object.
+chart = workbook.add_chart({'type': 'column'})
+
+# Configure the series of the chart from the dataframe data.
+chart.add_series({'values': '=Sheet1!$B$2:$B$8'})
+
+# Insert the chart into the worksheet.
+worksheet.insert_chart('D2', chart)
+
+#--------------------- 2nd part experimenting
+
+import pandas as pd
+
+# Create a Pandas dataframe from the data.
+df = pd.DataFrame({'Data': [10, 20, 30, 20, 15, 30, 45]})
+
+# Create a Pandas Excel writer using XlsxWriter as the engine.
+writer = pd.ExcelWriter('MyChart.xlsx', engine='xlsxwriter')
+
+# Convert the dataframe to an XlsxWriter Excel object.
+df.to_excel(writer, sheet_name='Sheet1')
+
+# Get the xlsxwriter objects from the dataframe writer object.
+workbook  = writer.book
+worksheet = writer.sheets['Sheet1']
+
+# Create a line object.
+chart = workbook.add_chart({'type': 'line'})
+
+# Configure the series of the chart from the dataframe data.
+chart.add_series({'values': '=Sheet1!$B$2:$B$8'})
+
+# Insert the chart into the worksheet.
+worksheet.insert_chart('D2', chart)
